ee7c54bd0c40f9f615c1536f8741930590d9d80d
MarioPabloJesus <mpjabd@gmail.com>
2016-06-19 16:39:03 +0000

    Contador agregado, usuarios pueden crear usuarios, faltan relacionarlos

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+before_action :configure_account_update_params, only: [:update]
+skip_before_action :require_no_authentication
 
   # GET /resource/sign_up
   # def new
@@ -40,13 +41,18 @@ before_action :configure_sign_up_params, only: [:create]
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :type, :counter])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname, :type, :counter])
+  end
+  
+  #def after_sign_in_path_for(resource)
+    #perfil_path
+  #  new_user_registration_path
+  #end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617011617) do
+ActiveRecord::Schema.define(version: 20160619025245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160617011617) do
     t.string   "name"
     t.string   "surname"
     t.string   "type"
+    t.integer  "counter"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
