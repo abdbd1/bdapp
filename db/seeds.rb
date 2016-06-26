cf95020b667eaf3a5d5e998f2ead66de0153225d
MarioPabloJesus <mpjabd@gmail.com>
2016-06-26 04:00:20 +0000

    Se agregaron algunos seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([
+  {name: 'Jesús', surname: 'Di Bari', email: 'iijbari@gmail.com', password: 'password', type: 'Principal', counter: 1},
+  {name: 'Jesús', surname: 'Administrador', email: 'jesus@admin.com', password: 'password', type: 'Administrador', counter: 2, parent_id: 1},
+  {name: 'Jesús', surname: 'Secundario', email: 'jesus@secundario.com', password: 'password', type: 'Secundario', counter: 3, parent_id: 1},
+])
